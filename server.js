757d344144cc23c09e899f4e954416494c7fc659
KwongTN <tungnan5636@gmail.com>
2020-01-17 21:01:52 +0800

    Create index.html

--- server.js
+++ server.js
@@ -48,5 +48,65 @@ var server = https.createServer(serverOptions, (req, res) => {
 
 });
 
+// Load socket.io
+var io = require('socket.io').listen(server);
+
+// Alert console when connected
+io.sockets.on('connection', (socket) => {
+    socket.emit("message", "You are connected. Welcome.");
+    console.log("[" + new Date() + "] " + ++clientCount + " connection attempts since server start.");
+
+    // To check if code is taken.
+    socket.on("codeCheck", (check) => {
+        socket.emit("codeVerification", sessionKeys.hasOwnProperty(check));
+    });
+
+    // Server generates a sessionID and responses
+    socket.on("getSessionID", () => {
+        socket.sessionID = 0;
+        socket.exists = true;
+
+        do {
+            socket.sessionID = /.*(?=\.)/.exec(Math.random() * 10000).toString();
+            if (sessionKeys.hasOwnProperty(socket.sessionID)) {
+                socket.exists = true;
+            } else {
+                sessionKeys[socket.sessionID] = "";
+                socket.exists = false;
+            }
+
+        } while (socket.exists)
+        console.log("Given sessionID: " + socket.sessionID);
+        socket.emit("acceptSessionID", socket.sessionID);
+    });
+
+    // To receive and respond session keys.
+    socket.on("codeSubmit", (submission) => {
+        sessionKeys[submission.ID.toString()] = submission.key.toString();
+        socket.broadcast.emit("codeResponse", {sessionID: submission.ID.toString(), sessionKey: submission.key.toString() });
+        console.log(submission);
+        console.log(sessionKeys);
+    });
+
+    // To check and respond session keys to requester.
+    socket.on("codeQuery", (request) => {
+        console.log(sessionKeys);
+        try {
+            socket.emit("codeResponse", { sessionID: request.toString(), sessionKey: sessionKeys[request].toString() });
+        } catch (err) {
+            socket.emit("codeResponse", { sessionID: request, sessionKey: "error"});
+            console.log("Probably value does not exist.");
+        }
+        console.log(request + " : " + sessionKeys[request]);
+    });
+
+    // Poke functions
+    socket.on("pokeQuery", (response) => {
+        socket.emit("pokeResponse", response);
+        console.log("Poke " + ++pokeCount + " from " + response);
+    });
+});
+
+
 
 server.listen(8080);
